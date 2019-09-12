1a4a4dda030c14342fdb978c1c85ab1ef24e7e82
Michelle Duer <michelle.duer@gmail.com>
2019-09-11 21:26:37 -0700

    add ignore file, setup basic directory structure
    hello world
    
    sample unit test

---
+++ BankingLedger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingLedger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
