1206e7f8cb7c4dbedfa99d7442cc2b8f3d4140be
Michelle Duer <michelle.duer@gmail.com>
2019-09-12 16:02:00 -0700

    add deposit method, create ledger object, wip: transaction methods

--- BankingLedger/Account.cs
+++ BankingLedger/Account.cs
@@ -1,20 +1,79 @@
 using System;
+using System.Collections.Generic;
 
 namespace BankingLedger
 {
+    // The Account class handles the balance and ledger attributes 
+    // while providing methods and their wrappers for modifying 
+    // and displaying the attributes
     public class Account
     {
-        private double balance;
+        private double _balance;
+
+        private List<Transaction> _ledger;
+
         public double Balance {
-            get { return balance; }
-            set { balance = value; }
+            get { return _balance; }
+            set { _balance = value; }
         }
 
         public Account(double startingBalance = 0) 
         {
-            balance = startingBalance;
+            this._balance = startingBalance;
+            this._ledger = new List<Transaction>();
         }
 
+        // wrapper: deposit the parameterized amount into the balance
+        public bool deposit(double amount)
+        {
+            return _deposit(amount);
+        }
 
+        // wrapper: add the new transaction into the ledger
+        public bool addTransaction(Transaction activity)
+        {
+            return _addTransaction(activity);
+        }
+
+        // display the resulting balance
+        public void viewBalance() {
+            Console.WriteLine($"BALANCE: ${Balance}");  // TODO: check that this value updates correctly 
+        }
+
+        // deposit the parameterized amount into the balance
+        // and record the transaction in the ledger
+        private bool _deposit(double amount)
+        {
+            if (amount < 0) {
+                return false;
+            }
+
+            this._balance += amount;
+            this._recordTransaction(amount, "deposit");
+            return true;
+        }
+
+        // record the transaction in the ledger
+        private bool _recordTransaction(double amount, string type)
+        {
+            if (amount < 0) {
+                return false;
+            }
+            Transaction activity = new Transaction(amount, type);
+
+            return true;
+        }
+
+        // add the new transaction into the ledger
+        private bool _addTransaction(Transaction activity)
+        {
+            if (activity == null) {
+                return false;
+            }
+
+            // TODO: add transaction 
+            Console.WriteLine("Need to implement adding transaction");
+            return true;
+        }
     }
 }
---
+++ BankingLedger/Transaction.cs
@@ -0,0 +1,28 @@
+namespace BankingLedger
+{
+    // The Transaction class represents a single transaction
+    // consisting of an amount and type of transaction
+    public class Transaction
+    {
+        private double _amount;
+        private string _type;
+
+        public double Amount
+        { 
+            get { return _amount; }
+            set { _amount = value; }
+        }
+
+        public string Type
+        { 
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        public Transaction(double amount, string type)
+        {
+            Amount = amount;
+            Type = type;
+        }
+    }
+}
