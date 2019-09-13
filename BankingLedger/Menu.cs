719136c36334c542ce4afe3deb07527438a3f1c8
Michelle Duer <michelle.duer@gmail.com>
2019-09-13 11:16:33 -0700

    allow for additional prompts

--- BankingLedger/Menu.cs
+++ BankingLedger/Menu.cs
@@ -5,11 +5,15 @@ namespace BankingLedger
     // The Menu class prompts the user with menu options
     public static class Menu
     {
+        // A welcome message
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("\nWelcome to the Most Amazing Bank!\n");
+        }
 
         // A welcome menu that returns valid key stroke options
         public static ConsoleKey[] WelcomeMenu()
         {
-            Console.WriteLine("\nWelcome to the Most Amazing Bank!\n");
             Console.WriteLine("Please select from the following options:");
             Console.WriteLine("(1) Login");
             Console.WriteLine("(2) Create Account");
@@ -19,5 +23,11 @@ namespace BankingLedger
             return new ConsoleKey[] {ConsoleKey.D1, ConsoleKey.D2, ConsoleKey.Escape};
         }
 
+
+        public static void Goodbye_TooManyInvalidKeyPresses()
+        {
+            Console.WriteLine("Too many invalid selections were made. Goodbye!");
+            Environment.Exit(1);
+        }
     }
 }
