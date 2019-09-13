04de3f4548b5accbc1ad6fe5408a249b4b67b0ac
Michelle Duer <michelle.duer@gmail.com>
2019-09-12 22:38:17 -0700

    add tests for adding transaction

--- BankingLedger.Tests/BankingLedger_TestAccount.cs
+++ BankingLedger.Tests/BankingLedger_TestAccount.cs
@@ -1,4 +1,6 @@
+using System;
 using Xunit;
+using System.Collections.Generic;
 
 namespace BankingLedger.UnitTests
 {
@@ -19,24 +21,54 @@ namespace BankingLedger.UnitTests
         }
 
         [Theory]
+        [InlineData(145)]
+        public void TestViewBalance_CorrectBalanceValue(double amount)
+        {
+            Account account = new Account();
+            Assert.Equal(0, account.Balance);
+
+            account.deposit(amount);
+            Assert.Equal(amount, account.Balance);
+        }
+
+        [Theory]
         [InlineData(0)]
         [InlineData(10)]
         [InlineData(746123484486.23498725)]
         [InlineData(999999999999.99999999)]
-        public void TestValidDeposit_CorrectBalanceIncrease(double money)
+        public void TestValidDeposit_CorrectBalanceIncrease(double amount)
         {
             Account account = new Account();
-            account.deposit(money);
-            Assert.Equal(money, account.Balance);
+            account.deposit(amount);
+            Assert.Equal(amount, account.Balance);
         }
 
         [Theory]
         [InlineData(-100.00)]
-        public void TestInvalidNegativeDeposit_BalanceRemainsZero(double money)
+        public void TestInvalidNegativeDeposit_BalanceRemainsZero(double amount)
         {
             Account account = new Account();
-            account.deposit(money);
+            account.deposit(amount);
             Assert.Equal(0, account.Balance);
+        }
+
+        [Theory]
+        [InlineData(15.50, "deposit")]
+        public void TestRecordTransaction_CorrectLedgerValues(double amount, string type)
+        {
+            Account account = new Account();
+            account.deposit(amount);
+            Assert.Equal(amount, account.Ledger[0].Amount);
+            Assert.Equal(type, account.Ledger[0].Type);
+        }
+
+        [Theory]
+        [InlineData(0, "deposit")]
+        public void TestInvalidRecordTransaction_EmptyLedger(double amount, string type)
+        {
+            Account account = new Account();
+            account.deposit(amount);
+            Assert.Empty(account.Ledger);
         }
 
     }
