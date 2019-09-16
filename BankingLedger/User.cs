ba81097d1d0d89a59a810a1fe266404d4fca4080
Michelle Duer <michelle.duer@gmail.com>
2019-09-15 19:32:06 -0700

    isolate User to hold user data only

--- BankingLedger/User.cs
+++ BankingLedger/User.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Text.RegularExpressions;
-
 namespace BankingLedger
 {
     public class User
@@ -50,94 +47,6 @@ namespace BankingLedger
             this.LastName = lastName;
             _secureHash = hash;
             _checking = new Account();
-        }
-
-        // user makes a withdrawal
-        public bool makeWithdrawal()
-        {
-            bool valid = false;
-            int promptCount = 0;
-            int maxPrompt = 5;
-            double amountFormatted;
-
-            do {
-                promptCount++;
-                Console.WriteLine("How much do you want to withdraw?");
-                Console.WriteLine("\nExample: 25.00\n");
-                string amount = Console.ReadLine();
-                amountFormatted = formatCurrency(amount);
-
-                if (amountFormatted == (double) -1) {
-                    Console.WriteLine("Invalid value was entered.");
-                } else {
-                    valid = true;
-                }
-            } while (!valid && promptCount <= maxPrompt);
-
-            if (_checking.Balance - amountFormatted < 0) {
-                Console.WriteLine($"\nWithdrawing {amountFormatted:C} will overdraw your account.");
-                Console.WriteLine("Would you like to continue with the withdrawal? (y/n)");
-                string response = Console.ReadLine().ToUpper();
-                char confirmation = response[0];
-
-                if (!confirmation.Equals('Y')) {
-                    Console.WriteLine("\nTransaction Cancelled");
-                    return false;
-                }
-            }
-
-            return _checking.withdraw(amountFormatted);
-        }
-
-        // user makes a deposit
-        public bool makeDeposit()
-        {
-            bool valid = false;
-            int promptCount = 0;
-            int maxPrompt = 5;
-            double amountFormatted;
-
-            do {
-                promptCount++;
-                Console.WriteLine("How much do you want to deposit?");
-                Console.WriteLine("\nExample: 25.00\n");
-                string amount = Console.ReadLine();
-                amountFormatted = formatCurrency(amount);
-
-                if (amountFormatted == (double) -1) {
-                    Console.WriteLine("Invalid value was entered.");
-                } else {
-                    valid = true;
-                }
-            } while (!valid && promptCount <= maxPrompt);
-
-            return _checking.deposit(amountFormatted);
-        }
-
-        // format string into currency
-        public double formatCurrency(string amountString)
-        {
-            double formatted = (double) -1;
-            if (string.IsNullOrEmpty(amountString))
-            {
-                return formatted;
-            }
-
-            // Check that the parameter has a valid pattern
-            string pattern = @"^$?\s*\d*.\d*\s*$";
-            Regex regex = new Regex(pattern);
-
-            // If the string is valid, convert to a double
-            if (regex.IsMatch(amountString)) {
-                try {
-                    formatted = Convert.ToDouble(amountString);
-                } catch (FormatException) {
-                    Console.WriteLine($"Unable to convert '{amountString}' to a valid transaction value.");
-                } catch (OverflowException) {
-                    Console.WriteLine($"{amountString} is outside a double type range");
-                }
-            }
-            return formatted;
         }
     }
 }
